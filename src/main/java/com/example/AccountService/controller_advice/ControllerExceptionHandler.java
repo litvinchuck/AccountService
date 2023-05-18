d0d835ed213aa7fefe198c4603c9c872ea66f83b
Eduard Litvinchuk <elitvinchuck@gmail.com>
2023-05-18 13:17:53 +0300

    Fix error response not displaying field name

--- src/main/java/com/example/AccountService/controller_advice/ControllerExceptionHandler.java
+++ src/main/java/com/example/AccountService/controller_advice/ControllerExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -21,9 +22,10 @@ public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
     protected ResponseEntity<Object> handleMethodArgumentNotValid(
             MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
 
-        String message = ex.getAllErrors()
+        String message = ex.getBindingResult().getFieldErrors()
                 .stream()
-                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .map(FieldError::getDefaultMessage)
+//                .map((fieldError -> fieldError.getField() + ": " + fieldError.getDefaultMessage()))
                 .collect(Collectors.joining(", "));
 
         ExceptionResponse body = ExceptionResponse.builder()
