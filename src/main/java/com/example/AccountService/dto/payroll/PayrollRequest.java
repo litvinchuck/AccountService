54d151f265099cef8728cfa5edba78363bb7356b
Eduard Litvinchuk <elitvinchuck@gmail.com>
2023-06-03 16:36:05 +0300

    payroll request validation

--- src/main/java/com/example/AccountService/dto/payroll/PayrollRequest.java
+++ src/main/java/com/example/AccountService/dto/payroll/PayrollRequest.java
@@ -2,6 +2,9 @@ package com.example.AccountService.dto.payroll;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,12 +18,15 @@ import java.time.YearMonth;
 @Builder
 public class PayrollRequest {
 
+    @PositiveOrZero(message = "salary property can not be less than 0")
     private Long salary;
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM-yyyy")
     private YearMonth period;
 
     @JsonProperty("employee")
+    @NotBlank(message = "employee property should not be blank")
+    @Email(regexp = ".+@acme\\.com$", message = "email should be registered in acme.com domain")
     private String employeeEmail;
 
 }
